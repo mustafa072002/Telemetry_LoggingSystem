24d1127bd8ac7941b8ed62f8e6dc11910e465439
mustafa07072002 <mustafamahgoub.2002@gmail.com>
2026-03-01 22:25:52 +0200

    implemented Safe File and Socket

---
+++ include/Telemetry/FileTelemetrySourceImpl.hpp
@@ -0,0 +1,13 @@
+#ifndef FILE_TELEMETRY_SOURCE_IMPL_HPP
+#define FILE_TELEMETRY_SOURCE_IMPL_HPP
+
+#include "ITelemetrySource.hpp"
+
+class FileTelemetrySourceImpl : public ITelemetrySource
+{
+public:
+
+};
+
+
+#endif // FILE_TELEMETRY_SOURCE_IMPL_HPP
