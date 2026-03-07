5c025e28e5fe49bf006536be8e6d2c17578e4666
mustafa07072002 <mustafamahgoub.2002@gmail.com>
2026-03-07 05:02:05 +0200

    Edit

--- include/Telemetry/FileTelemetrySourceImpl.hpp
+++ include/Telemetry/FileTelemetrySourceImpl.hpp
@@ -13,26 +13,26 @@ private:
 public:
     FileTelemetrySourceImpl(const std::string &path);
 
-    /*     // To Apply Rule of Zero u should let the compiler generate them implicitly
+    // /*     // To Apply Rule of Zero u should let the compiler generate them implicitly
 
-        // Default Constructor
-        FileTelemetrySourceImpl() = delete;
+    //     // Default Constructor
+    //     FileTelemetrySourceImpl() = delete;
 
-        // Copy Constructor
-        FileTelemetrySourceImpl(const FileTelemetrySourceImpl &obj) = delete;
+    //     // Copy Constructor
+    //     FileTelemetrySourceImpl(const FileTelemetrySourceImpl &obj) = delete;
 
-        // Copy Assignment Operator
-        FileTelemetrySourceImpl &operator=(const FileTelemetrySourceImpl &obj) = delete;
+    //     // Copy Assignment Operator
+    //     FileTelemetrySourceImpl &operator=(const FileTelemetrySourceImpl &obj) = delete;
 
-        // Move Constructor
-        FileTelemetrySourceImpl(FileTelemetrySourceImpl &&obj) = default;
+    //     // Move Constructor
+    //     FileTelemetrySourceImpl(FileTelemetrySourceImpl &&obj) = default;
 
-        // Move Assignment Operator
-        FileTelemetrySourceImpl &operator=(FileTelemetrySourceImpl &&obj) = default;
+    //     // Move Assignment Operator
+    //     FileTelemetrySourceImpl &operator=(FileTelemetrySourceImpl &&obj) = default;
 
-        // Destructor
-        ~FileTelemetrySourceImpl() = default;
-    */
+    //     // Destructor
+    //     ~FileTelemetrySourceImpl() = default;
+    // */
     bool openSource() override;
 
     bool readSource(std::string &out) override;
